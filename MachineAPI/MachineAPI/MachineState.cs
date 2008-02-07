a5c7204c06b45056bfb2d82426c4155beff123cc
anders <anders@f672c3ff-8b41-4f22-a4ab-2adcb4f732c7>
2008-02-07 16:06:38 +0000

    added the event
    git-svn-id: file:///home/srv/www/butt.se/pnp/svn/trunk@6 f672c3ff-8b41-4f22-a4ab-2adcb4f732c7

---
+++ MachineAPI/MachineAPI/MachineState.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MachineController
+{
+	public class MachineState
+	{
+		public uint x
+		{
+			get
+			{
+				throw new System.NotImplementedException();
+			}
+			set
+			{
+			}
+		}
+
+		public uint y
+		{
+			get
+			{
+				throw new System.NotImplementedException();
+			}
+			set
+			{
+			}
+		}
+
+		public uint z
+		{
+			get
+			{
+				throw new System.NotImplementedException();
+			}
+			set
+			{
+			}
+		}
+
+		public bool IsLegal { get; set; }
+
+	}
+}
