1f7b02ca879db9fbdbea0b5cb5f76ddf46a017a0
henmak-4 <henmak-4@f672c3ff-8b41-4f22-a4ab-2adcb4f732c7>
2008-02-07 18:01:56 +0000

    git-svn-id: file:///home/srv/www/butt.se/pnp/svn/trunk@7 f672c3ff-8b41-4f22-a4ab-2adcb4f732c7

--- MachineAPI/MachineAPI/CommandQueue.cs
+++ MachineAPI/MachineAPI/CommandQueue.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Collections;
+using System.Threading;
 
 namespace MachineController
 {
@@ -12,6 +13,10 @@ namespace MachineController
 	{
 		private List<Command> queue;
 
+        public CommandQueue()
+        {
+            queue = new List<Command>();
+        }
 		public List<Command> Queue
 		{
 			get { return queue; }
@@ -19,17 +24,36 @@ namespace MachineController
 
 		public void Enqueue(Command cmd)
 		{
-			throw new System.NotImplementedException();
+            lock (this)
+            {
+                queue.add(cmd);
+                Monitor.pulse(this);
+            }
 		}
 
-		public Command Dequeue()
-		{
-			throw new System.NotImplementedException();
-		}
+        public Command Dequeue()
+        {
+            lock (this)
+            {
+                while (true)
+                {
+                    if (queue.Count == 0)
+                    {
+                        Monitor.Wait(this);
+                    }
+                    else
+                    {
+                        Command cmd = queue.Item(0);
+                        queue.RemoveAt(0);
+                        return cmd;
+                    }
+                }
+            }
+        }
 
 		public void Clear()
 		{
-			throw new System.NotImplementedException();
+            queue.Clear();
 		}
 	}
 }
