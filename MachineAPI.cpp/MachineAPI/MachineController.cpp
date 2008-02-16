d9dc544cb6f11a04c467a2db83d221fdaadd5620
henmak-4 <henmak-4@f672c3ff-8b41-4f22-a4ab-2adcb4f732c7>
2008-02-16 10:59:38 +0000

    git-svn-id: file:///home/srv/www/butt.se/pnp/svn/trunk@17 f672c3ff-8b41-4f22-a4ab-2adcb4f732c7

---
+++ MachineAPI.cpp/MachineAPI/MachineController.cpp
@@ -0,0 +1,22 @@
+#include "MachineController.h"
+
+MachineController::MachineController(string comPort)
+{
+	this.comPort = comPort;
+}
+
+MachineController::~MachineController(void)
+{
+}
+
+void start(void) 
+{
+}
+
+void stop(void)
+{
+}
+
+bool runCommand(MachineCommand)
+{
+}
