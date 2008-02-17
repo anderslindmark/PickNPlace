88b2d5c6d86e3d564d1b78f55afcd9829cda8e20
henmak-4 <henmak-4@f672c3ff-8b41-4f22-a4ab-2adcb4f732c7>
2008-02-17 16:53:07 +0000

    git-svn-id: file:///home/srv/www/butt.se/pnp/svn/trunk@27 f672c3ff-8b41-4f22-a4ab-2adcb4f732c7

--- MachineAPI/MachineCommand.h
+++ MachineAPI/MachineCommand.h
@@ -5,8 +5,8 @@ using namespace std;
 class MachineCommand {
 public:
 	//MachineCommand();
-	//virtual ~MachineCommand();
-//	virtual void getAfterState(MachineState) = 0;
+	virtual ~MachineCommand() = 0 {};
+	//virtual void getAfterState(MachineState) = 0;
 	virtual int getCommand() = 0;
 	//virtual string getReadyRegister() = 0;
 	//virtual string getReadyValue() = 0;
