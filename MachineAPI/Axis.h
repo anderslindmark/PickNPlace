b49d4cbbd099524cce49a65934fa73516a179e98
henmak-4 <henmak-4@f672c3ff-8b41-4f22-a4ab-2adcb4f732c7>
2008-02-25 16:38:22 +0000

    doxygen
    git-svn-id: file:///home/srv/www/butt.se/pnp/svn/trunk@52 f672c3ff-8b41-4f22-a4ab-2adcb4f732c7

--- MachineAPI/Axis.h
+++ MachineAPI/Axis.h
@@ -1,6 +1,23 @@
+/**
+ 	\file Axis.h
+ 
+ 	\brief
+ 	Contains the Axis enum
+ 
+
+ 	\author	Henrik Mäkitaavola & Anders Lindmark
+**/
+
+#ifndef __AXIS_H__
+#define __AXIS_H__
+
+/// \enum
+/// \brief The different axis the Pick n Place machine can be moved in
 enum Axis
 {
 	AXIS_X,
 	AXIS_Y,
 	AXIS_Z
 };
+
+#endif //__AXIS_H__
