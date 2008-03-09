21fdd7c5a02e77cf94b222150ab2ef49cc8aa9b6
js <js@f672c3ff-8b41-4f22-a4ab-2adcb4f732c7>
2008-03-09 01:43:37 +0000

    Added a new project for the CameraWidget. Since using a widget in the designer requires a dll + we want a .lib to link against (that is, 2 targets), I'm NOT using the Visual Studio project when building it, instead I'm using the Makefile generated by qmake.exe (a QT app). The makefiles will build both a .dll and a .lib and then install it in the QT dir so that it can be used with the designer. I automated this in the build for the main GUI project using a custom pre-build step that runs a .bat file that in turn generates the makefile + builds it using nmake (Microsofts own make)...
    git-svn-id: file:///home/srv/www/butt.se/pnp/svn/trunk@87 f672c3ff-8b41-4f22-a4ab-2adcb4f732c7

---
+++ GUI/CameraWidget/CameraWidgetPlugin.cpp
@@ -0,0 +1,122 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-2007 Trolltech ASA. All rights reserved.
+**
+** This file is part of the example classes of the Qt Toolkit.
+**
+** This file may be used under the terms of the GNU General Public
+** License version 2.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of
+** this file.  Please review the following information to ensure GNU
+** General Public Licensing requirements will be met:
+** http://trolltech.com/products/qt/licenses/licensing/opensource/
+**
+** If you are unsure which license is appropriate for your use, please
+** review the following information:
+** http://trolltech.com/products/qt/licenses/licensing/licensingoverview
+** or contact the sales department at sales@trolltech.com.
+**
+** In addition, as a special exception, Trolltech gives you certain
+** additional rights. These rights are described in the Trolltech GPL
+** Exception version 1.0, which can be found at
+** http://www.trolltech.com/products/qt/gplexception/ and in the file
+** GPL_EXCEPTION.txt in this package.
+**
+** In addition, as a special exception, Trolltech, as the sole copyright
+** holder for Qt Designer, grants users of the Qt/Eclipse Integration
+** plug-in the right for the Qt/Eclipse Integration to link to
+** functionality provided by Qt Designer and its related libraries.
+**
+** Trolltech reserves all rights not expressly granted herein.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include "CameraWidget.h"
+#include "CameraWidgetPlugin.h"
+
+#include <QtPlugin>
+
+CameraWidgetPlugin::CameraWidgetPlugin(QObject *parent)
+    : QObject(parent)
+{
+    initialized = false;
+}
+
+void CameraWidgetPlugin::initialize(QDesignerFormEditorInterface * /* core */)
+{
+    if (initialized)
+        return;
+
+    initialized = true;
+}
+
+bool CameraWidgetPlugin::isInitialized() const
+{
+    return initialized;
+}
+
+QWidget *CameraWidgetPlugin::createWidget(QWidget *parent)
+{
+    return new CameraWidget(parent);
+}
+
+QString CameraWidgetPlugin::name() const
+{
+    return "Camera Widget";
+}
+
+QString CameraWidgetPlugin::group() const
+{
+    return "Pick n Place";
+}
+
+QIcon CameraWidgetPlugin::icon() const
+{
+    return QIcon();
+}
+
+QString CameraWidgetPlugin::toolTip() const
+{
+    return "";
+}
+
+QString CameraWidgetPlugin::whatsThis() const
+{
+    return "";
+}
+
+bool CameraWidgetPlugin::isContainer() const
+{
+    return false;
+}
+
+QString CameraWidgetPlugin::domXml() const
+{
+    return "<widget class=\"CameraWidget\" name=\"CameraWidget\">\n"
+           " <property name=\"geometry\">\n"
+           "  <rect>\n"
+           "   <x>0</x>\n"
+           "   <y>0</y>\n"
+           "   <width>100</width>\n"
+           "   <height>100</height>\n"
+           "  </rect>\n"
+           " </property>\n"
+           " <property name=\"toolTip\" >\n"
+           "  <string>The current time</string>\n"
+           " </property>\n"
+           " <property name=\"whatsThis\" >\n"
+           "  <string>The analog clock widget displays "
+           "the current time.</string>\n"
+           " </property>\n"
+           "</widget>\n";
+}
+
+QString CameraWidgetPlugin::includeFile() const
+{
+    return "CameraWidget.h";
+}
+
+Q_EXPORT_PLUGIN2(customwidgetplugin, CameraWidgetPlugin)
---
+++ GUI/CameraWidget/CameraWidgetPlugin.pro
@@ -0,0 +1,13 @@
+CONFIG      += designer plugin debug_and_release
+TARGET      = $$qtLibraryTarget($$TARGET)
+TEMPLATE    = lib
+QTDIR_build:DESTDIR     = $$QT_BUILD_TREE/plugins/designer
+
+HEADERS     = CameraWidget.h \
+              CameraWidgetPlugin.h
+SOURCES     = CameraWidget.cpp \
+              CameraWidgetPlugin.cpp
+
+# install
+target.path = $$[QT_INSTALL_PLUGINS]/designer
+INSTALLS += target
